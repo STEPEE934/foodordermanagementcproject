21f8422de088540c2a2be4ea275d7ff8390bb99a
STEPEE934 <steffissondawale@gmail.com>
2024-11-22 11:04:36 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class AreaOfTriangle
+{
+    public static void main (String args[])
+    {
+        double p,q;
+p=2;q=8;
+a=h*b/2;
+        System.out.println("area of triangle="+a);
+    }  
+    }
